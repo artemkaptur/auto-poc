1d3d10127253efdc0236b692fd5d915af63cf78f
Artsiom Kaptur <artsiom_kaptur@epam.com>
2019-09-04 11:39:34 +0300

    Added AddPetWithoutBodyTest

---
+++ src/test/java/tests/AddPetWithoutBodyTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static constants.AssertionErrorMessages.*;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddPetWithoutBodyTest extends BaseTest {
+
+    @Test
+    public void addPetWithoutBodyTest() {
+
+        Response addNewPetToTheStoreResponse = addNewPetToTheStore();
+        int statusCode = addNewPetToTheStoreResponse.statusCode();
+        String responseMessage = addNewPetToTheStoreResponse.body().jsonPath().getString("message");
+        String responseContentType = addNewPetToTheStoreResponse.contentType();
+
+        assertAll(
+                () -> assertEquals(statusCode, serverErrorStatusCode, format(STATUS_CODE_IS_NOT_ERRORED, statusCode)),
+                () -> assertEquals(responseContentType, applicationJsonContentType,
+                        format(WRONG_RESPONSE_CONTENT_TYPE, applicationJsonContentType, responseContentType)),
+                () -> assertTrue(addNewPetToTheStoreResponse.time() < maxResponseTime, TO_LONG_RESPONSE_TIME),
+                () -> assertEquals(responseMessage, serverErrorMessage)
+        );
+    }
+
+}
